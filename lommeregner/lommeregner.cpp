05ea1832f78eaf31579d84f5256425e8ef84e71c
Kenneth Gorking <kgorking@outlook.com>
2025-02-28 12:15:27 +0100

    Ryddet op i koden

--- lommeregner/lommeregner.cpp
+++ lommeregner/lommeregner.cpp
@@ -2,33 +2,17 @@
 #include <iostream>
 #include <unordered_map>
 
-double add(double a, double b) { return a + b; }
-double sub(double a, double b) { return a - b; }
-double mul(double a, double b) { return a * b; }
-double div(double a, double b) { return a / b; }
-double power(double a, double b) { return std::powl(a, b); }
-double sqrt(double a, double b) { return std::powl(b, 1.0 / a); }
-
 static auto const ops = std::unordered_map<char, double(*)(double, double)>{
-    {'+', add},
-    {'-', sub},
-    {'*', mul},
-    {'x', mul},
-    {'/', div},
-    {'^', power},
-    {'v', sqrt},
+    {'+', +[](double a, double b) { return a + b; }},
+    {'-', +[](double a, double b) { return a - b; }},
+    {'*', +[](double a, double b) { return a * b; }},
+    {'/', +[](double a, double b) { return a / b; }},
+    {'^', +[](double a, double b) { return std::pow(a, b); }},
+    {'v', +[](double a, double b) { return std::pow(b, 1.0 / a); }}
 };
 
 int main(int argc, char* argv[]) {
-    std::println(R"(
-Lommeregner, Gorking stil.
-  3 + 5 = 8
-  3 - 5 = -2
-  3 * 5 = 15
-  3 / 5 = 0.6
-  10 ^ 3 = 1000
-  3 v 1000 = ~10
-)");
+    std::println("Lommeregner, Gorking stil.");
     std::println("Angiv [tal] [+-*/^v] [tal]:");
 
     double num1 = 0, num2 = 0;
