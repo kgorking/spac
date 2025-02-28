b86cfab8bcf3591515012aa664ac92cb05db312b
Kenneth Gorking <kgorking@outlook.com>
2025-02-28 10:15:04 +0100

    Variant 2 og 3 færdig

--- lommeregner/lommeregner.cpp
+++ lommeregner/lommeregner.cpp
@@ -1,43 +1,49 @@
 #include <print>
 #include <iostream>
 
-void print_help() {
-    std::println("syntax: num1 op num2, hvor 'op' er +-*/");
-}
-
 int main(int argc, char* argv[]) {
     double num1 = 0, num2 = 0;
     char op = '?';
 
-    std::print("Angiv [tal] [+-*/] [tal]: ");
-    std::cin >> num1;
-    std::cin >> op;
-    std::cin >> num2;
+    std::println("Angiv [tal] [+-*/^v] [tal]:");
 
-    switch (op) {
-    case '+':
-        std::println("{} + {} = {}", num1, num2, num1 + num2);
-        break;
+    while (std::cin >> num1) {
+        std::cin >> op;
+        std::cin >> num2;
 
-    case '-':
-        std::println("{} - {} = {}", num1, num2, num1 - num2);
-        break;
+        switch (op) {
+        case '+':
+            std::println(" = {}", num1 + num2);
+            break;
 
-    case '*':
-        std::println("{} * {} = {}", num1, num2, num1 * num2);
-        break;
+        case '-':
+            std::println(" = {}", num1 - num2);
+            break;
 
-    case '/':
-        if (num2 == 0) {
-            std::println("Kan ikke dividere med nul");
+        case '*':
+            std::println(" = {}", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                std::println("Kan ikke dividere med nul");
+                return 1;
+            }
+            std::println(" = {}", num1 / num2);
+            break;
+
+        case '^':
+            std::println(" = {}", std::pow(num1, num2));
+            break;
+
+        case 'v':
+            std::println(" = {}", std::pow(num2, 1.0 / num1));
+            break;
+
+        default:
+            std::println("Ukendt operation '{}'", op);
             return 1;
         }
-        std::println("{} / {} = {}", num1, num2, num1 / num2);
-        break;
-
-    default:
-        std::println("Ukendt operation '{}'", op);
-        return 1;
     }
 
     return 0;
