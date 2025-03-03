2816796008b643d98011a48d30d02aacc38ee16b
Kenneth Gorking <kgorking@outlook.com>
2025-03-03 11:02:40 +0100

    Generate random code

--- uge2/Mastermind/main.cpp
+++ uge2/Mastermind/main.cpp
@@ -1,5 +1,34 @@
 #include <print>
+#include <array>
+#include <string_view>
+#include <ranges>
+#include <algorithm>
+#include <random>
+
+using namespace std::string_view_literals;
+
+constexpr auto colour_codes = std::to_array({
+	"\033[91m"sv, // rød
+	"\033[92m"sv, // grøn
+	"\033[93m"sv, // gul
+	"\033[94m"sv, // blå
+	"\033[95m"sv, // magenta
+	"\033[96m"sv, // cyan
+	});
+
+enum Colour {
+	Red, Green, Yellow, Blue, Magenta, Cyan
+};
+
+std::array<int,4> generate_code() {
+	std::array<int, 4> out;
+	std::ranges::generate_n(out.begin(), 4, [] { return std::rand() % (1+Cyan); });
+	return out;
+}
 
 int main() {
+	for (int i = 0; i < 10; i++)
+		std::println("{}", generate_code());
+
 	std::println("\033[91m test");
 }
