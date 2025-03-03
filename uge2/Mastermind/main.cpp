476f8b0ba91dabbc9aa94ef6e7224d46776f6633
Kenneth Gorking <kgorking@outlook.com>
2025-03-03 11:16:15 +0100

    Kan printe hjælp

--- uge2/Mastermind/main.cpp
+++ uge2/Mastermind/main.cpp
@@ -4,8 +4,11 @@
 #include <ranges>
 #include <algorithm>
 #include <random>
+#include <iostream>
 
 using namespace std::string_view_literals;
+
+using code = std::array<int, 4>;
 
 constexpr auto colour_codes = std::to_array({
 	"\033[91m"sv, // rød
@@ -16,21 +19,38 @@ constexpr auto colour_codes = std::to_array({
 	"\033[96m"sv, // cyan
 	});
 
-enum Colour {
-	Red, Green, Yellow, Blue, Magenta, Cyan
-};
 
-std::array<int,4> generate_code() {
-	std::array<int, 4> out;
-	std::ranges::generate_n(out.begin(), 4, [] { return std::rand() % (1+Cyan); });
+int generate_random_colour() {
+	return std::rand() % colour_codes.size();
+}
+
+code generate_code() {
+	code out;
+	std::ranges::generate_n(out.begin(), 4, generate_random_colour);
 	return out;
 }
 
-int main() {
-	for (int i = 0; i < 10; i++) {
-		auto const code = generate_code();
-		std::println("{}* {}* {}* {}*", colour_codes[code[0]], colour_codes[code[1]], colour_codes[code[2]], colour_codes[code[3]]);
-	}
+void print_code(code const c) {
+	std::println("{}* {}* {}* {}* \033[0m", colour_codes[c[0]], colour_codes[c[1]], colour_codes[c[2]], colour_codes[c[3]]);
+}
 
-	std::println("\033[91m test");
+code read_guess() {
+	std::print("Lav et gæt: ");
+
+	code out;
+	std::cin >> out[0] >> out[1] >> out[2] >> out[3];
+	return out;
+}
+
+void print_help() {
+	std::println(R"(Mastermind!
+	Tilladte farver er: {}R: rød, {}G :grøn, {}Y: gul, {}B: blå, {}M: magenta, {}C: cyan)",
+	colour_codes[0], colour_codes[1], colour_codes[2], colour_codes[3], colour_codes[4], colour_codes[5]);
+}
+
+int main() {
+	print_help();
+
+	auto const c = generate_code();
+	print_code(c);
 }
