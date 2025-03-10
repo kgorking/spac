039ce29ac58b552697a76ab9b66ca6f90e06e8b1
Kenneth Gorking <kgorking@outlook.com>
2025-03-10 10:41:05 +0100

    Flask server created

--- uge3/cereal_api/cereal_api.py
+++ uge3/cereal_api/cereal_api.py
@@ -1,6 +1,16 @@
 
 import import_csv
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    rows = import_csv.read("Cereal.csv")
+    names = next(rows)
+
+    return names
 
 
-for row in import_csv.read("Cereal.csv"):
-    print(row)
+
+app.run(host='0.0.0.0', port=81)
