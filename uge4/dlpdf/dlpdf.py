73a12f798166c95acb29776d83fcc9ef85e8333f
Kenneth Gorking <kgorking@outlook.com>
2025-03-21 08:07:21 +0100

    Cleanups

--- uge4/dlpdf/dlpdf.py
+++ uge4/dlpdf/dlpdf.py
@@ -1,21 +1,18 @@
 from genericpath import exists
-import ssl
-import urllib.parse
-from aiohttp.client import ClientTimeout
-import aiohttp.client_exceptions
-from aiohttp.cookiejar import CookieJar
-import pandas
-import asyncio
-import aiohttp
-import os
-import certifi
 from tqdm import tqdm
 from urllib.parse import urlparse
+import asyncio
+import aiohttp
+import ssl
+import urllib.parse
+import pandas
+import os
 
 excel_file = "./data/GRI_2017_2020 (1).xlsx"
 output_folder = "./download/"
 max_retries = 3
-url_timeout = 60.0
+url_timeout = 45.0
+developer_mode = False
 
 # Headers used to pretend we are a browser.
 # Some sites reject requests if they do not look like
@@ -25,9 +22,9 @@ headers = {
     "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Microsoft Edge";v="134"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": '"Windows"',
-    "Upgrade-Insecure-Requests": "1",
-    "Accept-encoding": "gzip, deflate, br, zstd",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    # "Upgrade-Insecure-Requests": "1",
+    "Accept-encoding": "gzip, deflate, zstd",
+    # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 }
 
 
@@ -44,12 +41,12 @@ def extract_report_names():
     excel = pandas.read_excel(excel_file, sheet_name=0)
     num_cells = len(excel["BRnum"].values)
 
-    rapport_names = zip(
+    report_names = zip(
         excel["BRnum"].values,
         excel["Pdf_URL"].values,
         excel["Report Html Address"].values,
     )
-    return rapport_names, num_cells
+    return report_names, num_cells
 
 
 async def download_file(session: aiohttp.ClientSession, tuple_data) -> str:
@@ -78,7 +75,7 @@ async def download_file(session: aiohttp.ClientSession, tuple_data) -> str:
             continue
 
         if retries.get(link, 0) == max_retries:
-            err = f"10\t{filename}\tretries exhausted: {link}\n"
+            err = f"retries exhausted: {link}\n"
             continue
 
         # Ignore local files
@@ -90,14 +87,15 @@ async def download_file(session: aiohttp.ClientSession, tuple_data) -> str:
             link = link[len('<a href="') :]
             link = link[: link.find('"')]
 
-        # Fix unicode 'Full Stop'
-        link = link.replace("\x2e", ".")
+        # Fix address starting with a dot
+        if link[0] == ".":
+            link = link[1:]
 
         # Patch incomplete urls
         url = urlparse(link)
         if len(url.scheme) == 0:
             # print("patching: ", link)
-            links.insert(0, "ftp://" + link)
+            # links.insert(0, "ftp://" + link)
             links.insert(0, "http://" + link)
             links.insert(0, "https://" + link)
             continue
@@ -105,62 +103,58 @@ async def download_file(session: aiohttp.ClientSession, tuple_data) -> str:
         try:
             async with session.get(
                 link,
+                max_field_size=16 * 1024,  # 16k bytes response headers
                 headers=headers,
                 allow_redirects=True,
                 chunked=True,
-                # chunked=False,
+                timeout=aiohttp.ClientTimeout(
+                    sock_connect=url_timeout, sock_read=url_timeout
+                ),
                 # raise_for_status=True,
             ) as response:
-                if not response.content_type in [
+                accepted_content_types = [
                     "application/pdf",
                     "application/octet-stream",
-                ]:
-                    err = f"12\t{filename}\tnot a pdf: {response.content_type}\n"
+                    "binary/octet-stream",
+                ]
+
+                if not response.content_type in accepted_content_types:
+                    err = f"Link did not return a pdf for '{link}': {response.content_type}\n"
                     continue
 
-                if response.content_length and response.content_length <= 8192:
-                    err = f"13\t{filename}\tshit size pdf: {response.content_length}\n"
-                    continue
+                # if not response.content_length or response.content_length <= 8192:
+                #    err = f"13\t{filename}\tbad sized pdf for '{link}': {response.content_length}\n"
+                #    continue
 
                 try:
                     with open(outname, "wb") as file:
-                        # don't bother downloading the file, just fill it
-                        # with junk, the same size as the actual pdf
-                        file.write((response.content_length * "X").encode())
-                        # file.write(response.content)
-                    return f"0\t{filename}\n"
+                        # file.write((response.content_length * "X").encode())
+                        async for chunk in response.content.iter_any():
+                            file.write(chunk)
+
+                    if os.path.getsize(outname) > 0:
+                        err = ""
+                        break
+                    else:
+                        os.remove(outname)
+                        err = f"bad sized pdf\n"
+                except aiohttp.http_exceptions.ContentLengthError as cle:
+                    retries[link] = 1 + retries.get(link, 0)
+                    links.insert(0, link)
                 except Exception as e:
                     os.remove(outname)
-                    return f"X\t{filename}\tfile exception {e}\n"
-        except aiohttp.InvalidURL as e:
-            err = f"2\t{filename}\tinvalid url: '{e}'\n"
-        except aiohttp.InvalidUrlClientError as e:
-            err = f"3\t{filename}\tinvalid client url: '{e}'\n"
-        except aiohttp.RedirectClientError as e:
-            err = f"4\t{filename}\tredirect error: '{e}'\n"
-        except aiohttp.InvalidUrlRedirectClientError as e:
-            err = f"5\t{filename}\tmalformed url error: '{e}'\n"
-        except aiohttp.NonHttpUrlClientError as e:
-            err = f"6\t{filename}\tnon-http client error: '{e}'\n"
-        except aiohttp.NonHttpUrlRedirectClientError as e:
-            err = f"7\t{filename}\tnon-http client or redirect error: '{e}'\n"
-        except aiohttp.ClientResponseError as e:
-            err = f"8\t{filename}\t{e}\n"
-        except aiohttp.ClientError as e:
-            err = f"9\t{filename}\tclient error: {e}\n"
-        except UnicodeEncodeError as e:
-            err = f"12\t{filename}\tunicode error: {e}\n"
-            pass
+                    err = f"file exception for '{link}' {e}\n"
+                    break
         except TimeoutError as e:
             retries[link] = 1 + retries.get(link, 0)
-            links.insert(0, link)  #
+            links.insert(0, link)
         except Exception as e:
-            err = f"11\t{filename}\tunknown error: {type(e)} - '{e}'\n"
+            err = f"{type(e)} - '{e}': '{link}'\n"
 
     if 0 == len(err):
-        return ""  # f"1\t{filename}"
+        return filename, None
     else:
-        return err
+        return filename, err
 
 
 async def download_all_files():
@@ -168,31 +162,34 @@ async def download_all_files():
         os.mkdir(output_folder)
 
     report_data, count = extract_report_names()
-    # report_data = [['test', 'https://www.bunge.com/sites/default/files/2018_gri_sustainability_report.pdf', None]]
-    # count = 1
 
-    ssl_ctx = ssl.create_default_context(cafile=certifi.where(), purpose=ssl.Purpose.SERVER_AUTH)
-    connector = aiohttp.TCPConnector(ssl=ssl_ctx) #, timeout_ceil_threshold=url_timeout)#, limit=50)
+    session = aiohttp.ClientSession(
+        cookie_jar=aiohttp.CookieJar(),
+        connector=aiohttp.TCPConnector(ssl=False, limit=500),
+        timeout=aiohttp.ClientTimeout(total=0),
+    )
 
-    async with aiohttp.ClientSession(
-        cookie_jar=CookieJar(),
-        connector=connector,
-        #timeout=ClientTimeout(total=url_timeout),
-    ) as session:
+    good = []
+    bad = []
+
+    async with session:
         tasks = [download_file(session, tuple_data) for tuple_data in report_data]
 
-        with open("report.csv", "wt") as report:
-            for f in tqdm(
-                asyncio.as_completed(tasks),
-                desc="Henter pdf filer",
-                unit=" pdf",
-                total=count,
-            ):
-                try:
-                    result = await f
-                    report.write(result)
-                except Exception as e:
-                    report.write("X: {e}")
+        for f in tqdm(asyncio.as_completed(tasks), unit="pdf", total=count):
+            try:
+                filename, error = await f
+                if not error:
+                    good.append(filename)
+                else:
+                    bad.append(filename + "\t" + error)
+            except Exception as e:
+                bad.append("async io error\t" + str(e))
+
+    with open("report.csv", "wt") as report:
+        for file in good:
+            report.write("0\t" + result)
+        for error in bad:
+            report.write("1\t" + error)
 
 
 if __name__ == "__main__":
